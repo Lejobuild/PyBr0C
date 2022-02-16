3cca892770a3df546590c2096282b6d01bb0c4bd
Lejobuild <78205960+Lejobuild@users.noreply.github.com>
2022-02-16 17:09:48 +0100

    Uploaded the View Bot Beta Version

---
+++ ViewBot.py
@@ -0,0 +1,15 @@
+import webbrowser
+from time import sleep
+#Its Only For Testing! Dont Use It For Creating Views!
+sleep(2)
+print("Its Only For Testing! Dont Use It For Creating Views!")
+sleep(5)
+print("DISCORD LejobuildBackup#7713")
+sleep(5)
+webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
+sleep(100)
+webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
+sleep(100)
+webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
+sleep(100)
+webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
