f61589c2806c43e1cc4dba0a0a783446e98ec193
Lejobuild <78205960+Lejobuild@users.noreply.github.com>
2022-02-16 17:41:04 +0100

    ViewBot.py Disk Usage Reducing
    New
    - Dish Usage reduced

--- ViewBot.py
+++ ViewBot.py
@@ -6,10 +6,15 @@ print("Its Only For Testing! Dont Use It For Creating Views!")
 sleep(5)
 print("DISCORD LejobuildBackup#7713")
 sleep(5)
-webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
-sleep(100)
-webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
-sleep(100)
-webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
-sleep(100)
-webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
+# Python program to illustrate
+# while loop
+count = 0
+while (count < 3):   
+    count = count + 1
+    webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
+    sleep(100)
+    webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
+    sleep(100)
+    webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
+    sleep(100)
+    webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
