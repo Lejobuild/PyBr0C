cd872849e92af81b5f27aeb171615baeb59a6230
Lejobuild <78205960+Lejobuild@users.noreply.github.com>
2022-02-16 20:09:40 +0100

    Update ViewBot.py

--- ViewBot.py
+++ ViewBot.py
@@ -1,17 +1,13 @@
 import webbrowser
 from time import sleep
-#Its Only For Testing! Dont Use It For Creating Views!
 sleep(2)
 print("Its Only For Testing! Dont Use It For Creating Views!")
 sleep(5)
-print("DISCORD LejobuildBackup#7713")
+print("DISCORD:LejobuildBackup#7713")
 sleep(5)
-# Python program to illustrate
 # while loop
 count = 0
 while (count < 3):   
     count = count + 1
-    webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
-    sleep(100)
     webbrowser.open_new_tab("https://www.youtube.com/watch?v=NwcXXqCxwpE")
     sleep(100)
